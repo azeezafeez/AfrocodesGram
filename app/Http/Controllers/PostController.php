d5cf4197ccdab9e6361c450c5fa2830d6b2231b8
azeezafeez <chillingloccin1@gmail.com>
2021-05-22 20:47:13 +0100

    final commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,9 +39,6 @@ class PostController extends Controller
             'caption'=>'required',
             'image'=>['required','image']
         ]);
-        // $imagePath= request('image')->store('uploads','public');
-        // $image= Image::make(public_path("storage/{$imagePath}"))->fit(1200,1200);
-        // $image->save();
         $post= auth()->user()->posts()->create([
             'caption'=> $data['caption'],
             'image'=> ""
@@ -52,20 +49,9 @@ class PostController extends Controller
 
     public function show(\App\Post $post){
         
-        // $comments=DB::table('users')
-        //     ->join('comments','users.id','=','comments.user_id')
-        //     ->join('profiles','profiles.user_id','=','users.id')
-        //     ->where('comments.post_id','=',$post->id)
-        //     ->orderBy('comments.id','DESC')->get();
-            
-          
         $commentCount= $post->comments()->count();
         
         $comments= Comment::where('post_id','=',$post->id)->with('user.profile')->orderBy('comments.id','DESC')->get();
-
-        // return Comment::all();
-        
-
         return view('posts.show',compact(['post','comments','commentCount']));
 
     }
